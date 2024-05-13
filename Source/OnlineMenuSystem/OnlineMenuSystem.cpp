29a4b61f78414775c68f34e809a42e395161ec1e
Void-11 <vivaanmishra777@gmail.com>
2024-05-14 02:05:31 +0530

    Project Created
    -Configured for Steam using Online Subsystem Steam

---
+++ Source/OnlineMenuSystem/OnlineMenuSystem.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OnlineMenuSystem.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OnlineMenuSystem, "OnlineMenuSystem" );
+ 
