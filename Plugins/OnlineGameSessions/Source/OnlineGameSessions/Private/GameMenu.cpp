e6ae6cf8916a6bdf32b622c5b80e1ea4a8661a07
Void-11 <vivaanmishra777@gmail.com>
2024-05-22 23:55:22 +0530

    Update GameMenu.cpp

--- Plugins/OnlineGameSessions/Source/OnlineGameSessions/Private/GameMenu.cpp
+++ Plugins/OnlineGameSessions/Source/OnlineGameSessions/Private/GameMenu.cpp
@@ -7,18 +7,24 @@ void UGameMenu::MenuSetup()
 {
 	AddToViewport();
 	SetVisibility(ESlateVisibility::Visible);
-	bIsFocusable = true;
+	SetIsFocusable(true);;
 
-	UWorld* World = GetWorld();
-	if (World)
+	if (const auto World = GetWorld())
 	{
-		APlayerController* PlayerController = World->GetFirstPlayerController();
-		if (PlayerController)
+		if (const auto PlayerController = World->GetFirstPlayerController())
 		{
+			// Data structure used to set up an input mode that allows only the UI to
+			// respond to user input
 			FInputModeUIOnly InputModeData;
+
 			InputModeData.SetWidgetToFocus(TakeWidget());
+
+			// Don't lock mouse cursor to the viewport so mouse can move off-screen
 			InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+
 			PlayerController->SetInputMode(InputModeData);
+
+			// Make the mouse visible so user can see what they are doing
 			PlayerController->SetShowMouseCursor(true);
 		}
 	}
